9284fc10f0cce7c006d19245c04a67b512712445
Beau Hibbert <beauhibbert789@gmail.com>
2021-12-06 17:14:54 -0800

    added dependencies

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+
+constructor(props) {
+  super(props);
+  this.state = {
+    queryCity: ''
+  }
 }
 
-export default App;
+
+
+  render() {
+    return(
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <input type="text" placeholder="city name" name="city" />
+          <button type="submit">Explore!</button>
+        </form>
+      </div>
+    )
+  }
+}
