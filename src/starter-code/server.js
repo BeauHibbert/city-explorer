5a1c4ff91ec74e56bf4c2c24085953c8d738d750
Beau Hibbert <beauhibbert789@gmail.com>
2021-12-18 20:17:29 -0800

    fixing syntax of environmental variables

--- src/starter-code/server.js
+++ src/starter-code/server.js
@@ -1,23 +1,23 @@
-'use strict';
+// 'use strict';
 
-require('dotenv');
-const express = require('express');
-const cors = require('cors');
+// require('dotenv');
+// const express = require('express');
+// const cors = require('cors');
 
-const weather = require('./modules/weather.js');
-const app = express();
-app.use(cors());
+// const weather = require('./modules/weather.js');
+// const app = express();
+// app.use(cors());
 
-app.get('/weather', weatherHandler);
+// app.get('/weather', weatherHandler);
 
-function weatherHandler(request, response) {
-  const { lat, lon } = request.query;
-  weather(lat, lon)
-  .then(summaries => response.send(summaries))
-  .catch((error) => {
-    console.error(error);
-    response.status(200).send('Sorry. Something went wrong!')
-  });
-}  
+// function weatherHandler(request, response) {
+//   const { lat, lon } = request.query;
+//   weather(lat, lon)
+//   .then(summaries => response.send(summaries))
+//   .catch((error) => {
+//     console.error(error);
+//     response.status(200).send('Sorry. Something went wrong!')
+//   });
+// }  
 
-app.listen(process.env.PORT, () => console.log(`Server up on ${process.env.PORT}`));
+// app.listen(process.env.PORT, () => console.log(`Server up on ${process.env.PORT}`));
